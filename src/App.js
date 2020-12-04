25dfff70fa470c35222640206d60a1fc406e50ef
j-blalock <justin.m.blalock@gmail.com>
2020-12-04 01:30:22 -0600

    refactoring html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Single from "./pages/Single";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/category">
+        <Category />
+      </Route>
+      <Route path="/single">
+        <Single />
+      </Route>
+    </BrowserRouter>
   );
 }
 
