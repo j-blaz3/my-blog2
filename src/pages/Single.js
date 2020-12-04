eb28e2065e00472e66d66f3dd2e5ff5924501f42
j-blalock <justin.m.blalock@gmail.com>
2020-12-04 01:42:45 -0600

    additional refactoring

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
+import PostDesc from "../components/PostDesc";
+import Comment from "../components/Comment";
+import Footer from "../components/Footer";
 
 const Single = () => {
   return (
     <>
-      <div>Single !</div>
+      <Header />
+      <div className="single-post no-sidebar">
+        <PostDesc />
+        <Comment />
+      </div>
+      <Footer />
     </>
   );
 };
